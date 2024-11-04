970890e5f6795f634cf0e6ac93478418d4d6eed5
Chandra2k <117076931+Chandra1153@users.noreply.github.com>
2024-11-04 20:49:20 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.farmy;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");  // 로컬 파일 로드
+    }
+}
